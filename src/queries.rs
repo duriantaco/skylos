f91a81d23912495bea3f51fbbd3707ed514e994b
duriantaco <aaronoh2015@gmail.com>
2025-05-04 23:50:55 +0800

    attempting to fix the cross module import

--- src/queries.rs
+++ src/queries.rs
@@ -6,6 +6,8 @@ pub const IMPORT_QUERY: &str = r#"(import_statement) @import (import_from_statem
 pub const CALL_QUERY: &str = r#"
 (call function: (identifier) @call_func) @call
 (call function: (attribute object: (_) @object attribute: (identifier) @method_name)) @call
+(attribute object: (_) @attr_object attribute: (identifier) @attr_name) @attribute
+(call) @call_node
 "#;
 pub const DECORATOR_QUERY: &str = r#"
 (decorator (identifier) @decorator_name)
@@ -15,4 +17,13 @@ pub const MAIN_QUERY: &str = r#"(if_statement condition: (_) @cond consequence: 
 pub const ASSIGN_QUERY: &str = r#"
 (assignment left: (identifier) @var right: (call function: (identifier) @cls))
 (assignment left: (identifier) @var_method right: (attribute object: (identifier) @obj attribute: (identifier) @method))
+(assignment left: (attribute object: (_) @left_obj attribute: (identifier) @left_attr) right: (call function: (identifier) @right_cls))
+(assignment left: (attribute object: (_) @left_obj attribute: (identifier) @left_attr) right: (_) @right_value)
+(assignment) @assignment_node
+"#;
+pub const PROPERTY_QUERY: &str = r#"
+(expression_statement (attribute object: (_) @prop_object attribute: (identifier) @prop_name)) @property_access
+"#;
+pub const INSTANTIATION_QUERY: &str = r#"
+(call function: (identifier) @class_name arguments: (argument_list)) @instantiation
 "#;
