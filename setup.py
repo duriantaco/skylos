6a18033bc241d5ef477ed791a12dfd9163c76527
duriantaco <aaronoh2015@gmail.com>
2025-06-13 15:45:00 +0800

    bumped version to 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="skylos",
-    version="1.1.12",
+    version="1.2.0",
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=["inquirer>=3.0.0"],
