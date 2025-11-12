1464d25639b25b4a987299e46d828f2ca787bd7d
duriantaco <aaronoh2015@gmail.com>
2025-11-12 14:53:41 +0800

    bump version 2.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="skylos",
-    version="2.4.0",
+    version="2.5.0",
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=[
