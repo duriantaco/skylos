aa4fbff46862adfbdc6e69a176de83a388e53280
duriantaco <aaronoh2015@gmail.com>
2025-12-16 17:25:22 +0800

    test

--- app.py
+++ app.py
@@ -73,6 +73,13 @@ def zip_folder2():
     subprocess.run("ls -l " + path, shell=True)
     return {"ok": True}
 
+@app.get("/zip3")
+def zip_folder3():
+    path = request.args.get("path", ".")
+    os.system(f"zip -r out.zip {path}")
+    subprocess.run("ls -l " + path, shell=True)
+    return {"ok": True}
+
 @app.get("/fetch")
 def fetch():
     url = request.args.get("url", "http://127.0.0.1:80")
