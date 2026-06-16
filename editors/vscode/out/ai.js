2ed4353a89e237b9a2b2e7413c2595154407d034
duriantaco <aaronoh2015@gmail.com>
2026-06-16 10:29:37 +0800

    fix(vscode): use node16 module resolution

--- editors/vscode/out/ai.js
+++ editors/vscode/out/ai.js
@@ -1,4 +1,37 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AIAnalyzer = void 0;
 exports.extractFunctions = extractFunctions;
@@ -6,8 +39,8 @@ exports.callLLMStreamingWithCallback = callLLMStreamingWithCallback;
 exports.fixWithAI = fixWithAI;
 exports.callOpenAIStreaming = callOpenAIStreaming;
 exports.callAnthropicStreaming = callAnthropicStreaming;
-const vscode = require("vscode");
-const crypto = require("crypto");
+const vscode = __importStar(require("vscode"));
+const crypto = __importStar(require("crypto"));
 const config_1 = require("./config");
 const scanner_1 = require("./scanner");
 const verifyCore_1 = require("./verifyCore");
@@ -574,7 +607,7 @@ async function runPostFixValidation(command) {
     const ws = vscode.workspace.workspaceFolders?.[0];
     if (!ws)
         return { success: false, output: "No workspace folder" };
-    const { exec } = await Promise.resolve().then(() => require("child_process"));
+    const { exec } = await import("child_process");
     return new Promise((resolve) => {
         exec(command, { cwd: ws.uri.fsPath, timeout: 30000 }, (err, stdout, stderr) => {
             if (err) {
